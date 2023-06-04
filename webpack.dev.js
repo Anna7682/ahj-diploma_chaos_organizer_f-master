181845d6ef203e72d78d63acb218a0899c3aa8d2
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2023-06-04 20:25:08 +0300

    Update webpack.dev.js

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,13 +1,21 @@
-const merge = require('webpack-merge');
-
-const common = require('./webpack.common.js');
+const { merge } = require('webpack-merge');
+const webpack = require('webpack');
+const path = require('path');
+const common = require('./webpack.common');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
-},
-{
   devServer: {
-    port: 8080,
+    port: 9000,
+    historyApiFallback: true,
+    static: {
+      directory: path.join(__dirname, 'dist'),
+    },
+    open: true,
+    compress: true,
   },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
 });
