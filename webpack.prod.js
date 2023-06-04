45f497502920e20b5cdd8fd621e6a7d18bcb31b3
Anna7682 <107514746+Anna7682@users.noreply.github.com>
2023-06-04 20:24:41 +0300

    Update webpack.prod.js

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,21 +1,23 @@
-const merge = require('webpack-merge');
-
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-
-const TerserPlugin = require('terser-webpack-plugin');
-
+const { merge } = require('webpack-merge');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+const path = require('path');
 const common = require('./webpack.common');
-
-// const path = require('path');
-// const HtmlWebPackPlugin = require('html-webpack-plugin');
-// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
+  devtool: 'source-map',
   optimization: {
-    minimizer: [
-      new TerserPlugin({}),
-      new OptimizeCSSAssetsPlugin({}),
-    ],
+    minimize: true,
+    minimizer: [new CssMinimizerPlugin({}), '...'],
   },
+  plugins: [
+    new CopyPlugin({
+      patterns: [
+        {
+          from: path.resolve(__dirname, 'licence.txt'),
+        },
+      ],
+    }),
+  ],
 });
